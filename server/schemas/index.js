4ac9d642037cbcd82edb5d246e323bf14c167ca5
chilejay7 <codyburk7@yahoo.com>
2024-01-26 15:31:52 -0700

    schemas directory added with basic files

---
+++ server/schemas/index.js
@@ -0,0 +1,5 @@
+// These are exported to server.js to allow data requests to be properly routed.
+const { typeDefs } = require('./typeDefs');
+const { resolvers } = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
