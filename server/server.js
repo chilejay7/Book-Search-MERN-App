08ca6d9e5c2efe604a68f328c2ac54e2ecfe1ba6
chilejay7 <codyburk7@yahoo.com>
2024-01-26 15:19:50 -0700

    Apollo Server modules added and server.js restructured

--- server/server.js
+++ server/server.js
@@ -3,19 +3,41 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// Apollo server is imported.
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+// This creates a new instance of ApolloServer
+const startApolloServer = async () => {
+  await server.start();
+
+  // Express middleware is used to parse the data from incoming requests.
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  // Routes are used with the REST API.  Typedefs and resolvers will be used with GrpahQL in place of the routes.
+  // app.use(routes);
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`GraphQL can be accessed at http://localhost:${PORT}/graphql`);
+    });
+  });
+
+};
+
+startApolloServer();
