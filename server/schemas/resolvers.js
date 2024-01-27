2ad3e9944c2b0898d3e5c97e455d604adf2bd032
chilejay7 <codyburk7@yahoo.com>
2024-01-27 15:51:47 -0700

    me query updated in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,10 +3,15 @@ const { User, Book } = require('../models');
 const { signToken, Authenticator } = require('../utils/auth');
 
 const resolvers = {
-    // The query refrences our one query defined in typeDefs.js.  The savedBooks field is populated to pull in the information from the Books model.
+    // The query refrences our one query defined in typeDefs.js.  
+    // The savedBooks field is populated to pull in the information from the Books model.
     Query: {
-        me: async () => {
-            return User.find().populate('savedBooks')
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id}).populate('savedBooks');
+            }
+            throw AuthenticationError;
+           
         },
     },
 
