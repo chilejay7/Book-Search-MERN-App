1e7f56ccc4b9948810977ea2b033eb88515a84ce
chilejay7 <codyburk7@yahoo.com>
2024-01-27 16:09:27 -0700

    notes added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,6 +5,9 @@ const { signToken, Authenticator } = require('../utils/auth');
 const resolvers = {
     // The query refrences our one query defined in typeDefs.js.  
     // The savedBooks field is populated to pull in the information from the Books model.
+    // The parent parameter references the parent object, which isn't typically used in this resolver
+    // The args paramter references the args object which isn't typically used in this resolver
+    // The context object contains information about the authenticated user
     Query: {
         me: async (parent, args, context) => {
             if (context.user) {
