247f482e6db0d470d65eec900d153b62872fafdd
chilejay7 <codyburk7@yahoo.com>
2024-01-28 16:30:42 -0700

    untracked changes to mutations to correct variables added

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -10,7 +10,7 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-    mutation addUser(username: String!, email: String!, password: String!) {
+    mutation addUser($username: String!, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
             token
             user
@@ -19,7 +19,7 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-    mutation saveBook(authors:[String], description: String!, title: String!, bookId: String!, image: String!, link: String!) {}
+    mutation saveBook($authors:[String], $description: String!, $title: String!, $bookId: String!, $image: String!, $link: String!) {
     saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, link: $link){
         authors
         description
@@ -28,10 +28,11 @@ export const SAVE_BOOK = gql`
         image
         link
     }
+}
 `;
 
 export const REMOVE_BOOK = gql`
-    mutation removeBook(bookId: ID!) {
+    mutation removeBook($bookId: ID!) {
         removeBook(bookId: $bookId) {
             bookId
         }
