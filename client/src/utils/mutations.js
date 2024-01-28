7c0aede1cafb5b2e613db5694990ffc174fef200
chilejay7 <codyburk7@yahoo.com>
2024-01-28 14:54:20 -0700

    save book mutation added

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -16,4 +16,16 @@ export const ADD_USER = gql`
             user
         }
     }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook(authors:[String], description: String!, title: String!, bookId: String!, image: String!, link: String!) {}
+    saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, link: $link){
+        authors
+        description
+        title
+        bookId
+        image
+        link
+    }
 `
