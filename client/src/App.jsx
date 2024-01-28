4eca6a202253473d987fa0768c25b4119f750d6a
chilejay7 <codyburk7@yahoo.com>
2024-01-27 19:41:19 -0700

    Apollo provider created in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
+// The Apollo client is imported to allow requests through the client side appication to Apollo Server.
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
 
+// The Apollo Client is initialized.
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+// The Apollo Provider wraps the application's HTML elements
 function App() {
   return (
     <>
-      <Navbar />
-      <Outlet />
+      <ApolloProvider client={client}>
+        <Navbar />
+        <Outlet />
+      </ApolloProvider>
     </>
   );
 }
