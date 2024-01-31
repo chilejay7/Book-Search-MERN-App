566eacafffbf57eb2a0e0cb6bc3b86007055770d
chilejay7 <codyburk7@yahoo.com>
2024-01-30 22:58:14 -0700

    bcryptjs installed

--- server/server.js
+++ server/server.js
@@ -31,16 +31,6 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  // This middleware statement has been defined to help debug requests from the client and see the content being sent.
-  // app.use((req, res, next) => {
-  //   const { header } = req.headers;
-
-  //   console.log(`*************************************`);
-  //   console.log(`The request header is: ${header}`);
-  //   console.log(`*************************************`);
-  //   next();
-  // })
-
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
